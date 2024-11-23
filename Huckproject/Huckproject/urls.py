d63171b071826e7de927ab6d3f3d9314a5d4047d
Tokol1 <116242999+Tokol1@users.noreply.github.com>
2024-11-24 00:24:05 +0900

    エラー修正
    起動するとhome画面（仮）がでるように修正しました。

--- Huckproject/Huckproject/urls.py
+++ Huckproject/Huckproject/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from apps.home import views as home_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', home_views.index, name='home'),  # ルートURLに対応するビューを追加
     path('accounts/', include('apps.accounts.urls')),
     path('home/', include('apps.home.urls')),
     path('listings/', include('apps.listings.urls')),
