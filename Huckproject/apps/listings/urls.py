c320760e704d0fa7c6320f492fdf825f969f7c58
Tokol1 <116242999+Tokol1@users.noreply.github.com>
2024-12-02 18:06:18 +0900

    lis-error
    12/02/18:06エラー
    修正

--- Huckproject/apps/listings/urls.py
+++ Huckproject/apps/listings/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'listings'
-
 urlpatterns = [
-    path('', views.index, name='listings'),
+    path('', views.index, name='listings_index'),
 ]
--- Huckproject/apps/listings/views.py
+++ Huckproject/apps/listings/views.py
@@ -1,16 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .models import listings
 
-
-@login_required
-def listings(request):
-    admin_listings = listings.objects.filter(listings_type='admin')
-    user_listings = listings.objects.filter(user=request.user).exclude(listings_type='admin')
-    
-    context = {
-        'admin_listings': admin_listings,
-        'user_listings': user_listings,
-    }
-    return render(request, 'listings/listings.html', context)
-
+def index(request):
+    return render(request, 'listings/index.html')
