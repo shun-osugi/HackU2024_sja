af28607cc409cbc5ba0e6eaa948a8ba5080c3eaa
ShunOsugi <shun01.oh@gmail.com>
2024-12-08 16:53:07 +0900

    [back] マイページのバックエンド準備

--- Huckproject/apps/mypage/urls.py
+++ Huckproject/apps/mypage/urls.py
@@ -5,4 +5,8 @@ app_name = 'mypage'
 
 urlpatterns = [
     path('', views.index, name='mypage'),
+    # path('buy_record/', views.buy_record, name='buy_record'),#購入履歴
+    # path('member_info/', views.member_info, name='member_info'),#会員情報
+    # path('inquiry/', views.inquiry, name='inquiry'),#お問い合わせ
+    # path('listing_record/', views.listing_record, name='listing_record'),#出品履歴
 ]
