4ece98e995c36ed29d8b9a10f9381b949865003f
Tokol1 <116242999+Tokol1@users.noreply.github.com>
2024-12-20 15:19:35 +0900

    transactions
    statsの初期値を設定

--- Huckproject/apps/transactions/models.py
+++ Huckproject/apps/transactions/models.py
@@ -7,7 +7,11 @@ class Transaction(models.Model):
     seller = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='seller_transactions', on_delete=models.CASCADE)
     buyer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='buyer_transactions', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('completed', 'Completed'), ('cancelled', 'Cancelled')])
+    status = models.CharField(
+        max_length=20,
+        choices=[('pending', 'Pending'), ('completed', 'Completed'), ('cancelled', 'Cancelled')],
+        default='pending'  # 初期値を 'pending' に設定
+    )
     meeting_time = models.DateTimeField(null=True, blank=True)  # 新しいフィールド
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
